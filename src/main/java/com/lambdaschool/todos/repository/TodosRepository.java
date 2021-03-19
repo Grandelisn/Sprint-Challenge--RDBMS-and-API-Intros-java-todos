57451fe472e33f4097ae188a85e02f70cd6faa24
Grandelisn <grandelisn@gmail.com>
2021-03-19 18:11:31 -0400

    Create a CodeGrade submission

--- src/main/java/com/lambdaschool/todos/repository/TodosRepository.java
+++ src/main/java/com/lambdaschool/todos/repository/TodosRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface TodosRepository extends CrudRepository<Todos, Long>{
+public interface TodosRepository extends CrudRepository<Todos, Long> {
 
     @Transactional
     @Modifying
     @Query(value = "UPDATE todos SET completed = true, lastmodifiedby = :uname, lastmodifieddate = CURRENT_TIMESTAMP WHERE todoid = :todoid",
         nativeQuery = true)
-    void updateComplete(String uname,
-                        long todoid);
+    void updateComplete(
+        String uname,
+        long todoid);
 }
